7436ef90de77320d09d1d3744854a2b204720926
Aidennnn97 <aidennnn97@gmail.com>
2023-05-06 13:49:41 +0900

    호감표시 발생하면 알림 메세지 생성

--- src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/eventListener/NotificationEventListener.java
@@ -2,7 +2,8 @@ package com.ll.gramgram.boundedContext.notification.eventListener;
 
 import com.ll.gramgram.base.event.EventAfterLike;
 import com.ll.gramgram.base.event.EventAfterModifyAttractiveType;
-import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -12,16 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class NotificationEventListener {
-    private final InstaMemberService instaMemberService;
-
-    @EventListener
-    public void listen(EventAfterModifyAttractiveType event) {
-        // 누군가의 호감사유가 변경되었을 경우
-    }
+    private final NotificationService notificationService;
 
     @EventListener
     public void listen(EventAfterLike event) {
         // 누군가가 호감표시를 한 경우
+        LikeablePerson likeablePerson = event.getLikeablePerson();
+
+        notificationService.makeLike(likeablePerson);
+    }
+    @EventListener
+    public void listen(EventAfterModifyAttractiveType event) {
+        // 누군가의 호감사유가 변경되었을 경우
     }
 
 }
